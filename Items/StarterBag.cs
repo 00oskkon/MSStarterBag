50c44f5a12929cd07295d71da4af893c379fcb2a
00oskkon <00oskkon@protonmail.com>
2025-08-07 14:53:09 +0200

    Refactoring code

--- Items/StarterBag.cs
+++ Items/StarterBag.cs
@@ -7,6 +7,7 @@ using Terraria;
 using Terraria.GameContent.ItemDropRules;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.ModLoader.Config;
 using MSStarterBag.Common.Configs;
 
 namespace MSStarterBag.Items
@@ -14,6 +15,9 @@ namespace MSStarterBag.Items
 	// Create the StarterBag class, derived from ModItem and ILocalizedModType
 	public class StarterBag : ModItem, ILocalizedModType
 	{
+		// A list of the items to give the player
+		List<Item> storage = new();
+
 		// Static default values
 		public override void SetStaticDefaults() {
             		Item.ResearchUnlockCount = 0;
@@ -28,97 +32,63 @@ namespace MSStarterBag.Items
         	}
 
 		// You can right click the item
-        	public override bool CanRightClick() {
+        public override bool CanRightClick() {
 			return true;
+		}
+
+		// Function to add configured yes/no items to the list
+		private void AddBoolToList(bool instance, int item) {
+			if (instance) {
+				storage.Add(new(item));
+			}
+		}
+
+		// Function to add configured amounts of items to the list
+		private void AddIntToList(int instance, int item) {
+			if (instance > 0) {
+				for (int i = 0; i < instance; i++) {
+					storage.Add(new(item));
+				}
+			}
 		}
 
 		// When you right click the item
 		public override void RightClick(Player player) {
-			// A list of the items to give the player
-			List<Item> storage = new();
-
 			// Check the config if the player should get a Storage Heart and add to the list
-			if (BagConfig.Instance.GetStorageHeart) {
-				storage.Add(new(ModContent.ItemType<StorageHeart>()));
-			}
-			// Check the config if the player should get a Crafting Interface and add to the list
-			if (BagConfig.Instance.GetCraftingAccess) {
-				storage.Add(new(ModContent.ItemType<CraftingAccess>()));
-			}
+			AddBoolToList(BagConfig.Instance.GetStorageHeart, ModContent.ItemType<StorageHeart>());
 
-			// Warning!
-			// Everything below looks like crap
-			// I might clean it up...
+			// Check the config if the player should get a Crafting Interface and add to the list
+			AddBoolToList(BagConfig.Instance.GetCraftingAccess, ModContent.ItemType<CraftingAccess>());
 
 			// Add the configured amounts of Storage Units to the list
-			if (BagConfig.Instance.StorageUnitAmount > 0) {
-				for (int i = 0; i < BagConfig.Instance.StorageUnitAmount; i++) {
-					storage.Add(new(ModContent.ItemType<StorageUnit>()));
-				}
-			}
+			AddIntToList(BagConfig.Instance.StorageUnitAmount, ModContent.ItemType<StorageUnit>());
 
 			// Add the configured amounts of Crimtane Storage Upgrades to the list
-			if (BagConfig.Instance.CrimtaneAmount > 0) {
-				for (int i = 0; i < BagConfig.Instance.CrimtaneAmount; i++) {
-					storage.Add(new(ModContent.ItemType<UpgradeCrimtane>()));
-				}
-			}
+			AddIntToList(BagConfig.Instance.CrimtaneAmount, ModContent.ItemType<UpgradeCrimtane>());
 
 			// Add the configured amounts of Demonite Storage Upgrades to the list
-			if (BagConfig.Instance.DemoniteAmount > 0) {
-				for (int i = 0; i < BagConfig.Instance.DemoniteAmount; i++) {
-					storage.Add(new(ModContent.ItemType<UpgradeDemonite>()));
-				}
-			}
+			AddIntToList(BagConfig.Instance.DemoniteAmount, ModContent.ItemType<UpgradeDemonite>());
 
 			// Add the configured amounts of Hellstone Storage Upgrades to the list
-			if (BagConfig.Instance.HellstoneAmount > 0) {
-				for (int i = 0; i < BagConfig.Instance.HellstoneAmount; i++) {
-					storage.Add(new(ModContent.ItemType<UpgradeHellstone>()));
-				}
-			}
+			AddIntToList(BagConfig.Instance.HellstoneAmount, ModContent.ItemType<UpgradeHellstone>());
 
 			// Add the configured amounts of Hallowed Storage Upgrades to the list
-			if (BagConfig.Instance.HallowedAmount > 0) {
-				for (int i = 0; i < BagConfig.Instance.HallowedAmount; i++) {
-					storage.Add(new(ModContent.ItemType<UpgradeHallowed>()));
-				}
-			}
+			AddIntToList(BagConfig.Instance.HallowedAmount, ModContent.ItemType<UpgradeHallowed>());
 
 			// Add the configured amounts of Blue Chlorophyte Storage Upgrades to the list
-			if (BagConfig.Instance.ChlorophyteAmount > 0) {
-				for (int i = 0; i < BagConfig.Instance.ChlorophyteAmount; i++) {
-					storage.Add(new(ModContent.ItemType<UpgradeBlueChlorophyte>()));
-				}
-			}
+			AddIntToList(BagConfig.Instance.ChlorophyteAmount, ModContent.ItemType<UpgradeBlueChlorophyte>());
 
 			// Add the configured amounts of Luminite Storage Upgrades to the list
-			if (BagConfig.Instance.LuminiteAmount > 0) {
-				for (int i = 0; i < BagConfig.Instance.LuminiteAmount; i++) {
-					storage.Add(new(ModContent.ItemType<UpgradeLuminite>()));
-				}
-			}
+			AddIntToList(BagConfig.Instance.LuminiteAmount, ModContent.ItemType<UpgradeLuminite>());
 
 			// Add the configured amounts of Terra Storage Upgrades to the list
-			if (BagConfig.Instance.TerraAmount > 0) {
-				for (int i = 0; i < BagConfig.Instance.TerraAmount; i++) {
-					storage.Add(new(ModContent.ItemType<UpgradeTerra>()));
-				}
-			}
+			AddIntToList(BagConfig.Instance.TerraAmount, ModContent.ItemType<UpgradeTerra>());
 
 			// Add the configured amounts of Shadow Diamonds to the list
-			if (BagConfig.Instance.ShadowDiamondAmount > 0) {
-				for (int i = 0; i < BagConfig.Instance.ShadowDiamondAmount; i++) {
-					storage.Add(new(ModContent.ItemType<ShadowDiamond>()));
-				}
-			}
+			AddIntToList(BagConfig.Instance.ShadowDiamondAmount, ModContent.ItemType<ShadowDiamond>());
 
 			// Add the configured amounts of Storage Components to the list
-			if (BagConfig.Instance.ComponentAmount > 0) {
-				for (int i = 0; i < BagConfig.Instance.ComponentAmount; i++) {
-					storage.Add(new(ModContent.ItemType<StorageComponent>()));
-				}
-			}
+			AddIntToList(BagConfig.Instance.ComponentAmount, ModContent.ItemType<StorageComponent>());
 
 			// Add all the items from the list
 			foreach (Item item in storage) player.QuickSpawnItem(Item.GetSource_Loot(), item);
